ab678cc4949f8a5ade990e030472335c6343ddef
Radosav <radosavperisic.contact@gmail.com>
2024-12-19 10:49:32 +0100

    Enhanced the Services component with animations using Framer Motion and improved styles. Integrated assets like an animated sparks GIF and dynamic background to elevate visuals. Updated App.js to include the component in the main layout.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
+import Services from '../components/Services'
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <div>
         <Navbar />
         <Hero />
+        <Services />
       </div>
     </>
   );
