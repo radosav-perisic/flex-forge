ded00b071a7091a514437b744ce5b2fa1c23fcd1
radosav-perisic <ralepro123@gmail.com>
2024-12-28 20:21:24 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,26 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
-import Navbar from '../src/components/Navbar';
-import Hero from '../src/components/Hero';
-import Services from '../src/components/Services';
-import ServicesInfo from '../src/components/ServicesInfo';
-import CustomWebsite from '../src/pages/CustomWebsite';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "../src/components/Navbar";
+import Hero from "../src/components/Hero";
+import Services from "../src/components/Services";
+import ServicesInfo from "../src/components/ServicesInfo";
+import CustomWebsite from "../src/pages/CustomWebsite";
 
 function App() {
   return (
     <Router>
       <div>
-      <Navbar />
+        <Navbar />
         <Routes>
-
           <Route
             path="/"
             element={
               <>
-                
                 <Hero />
                 <Services />
                 <ServicesInfo />
               </>
             }
           />
-
 
           <Route path="/custom-website" element={<CustomWebsite />} />
         </Routes>
